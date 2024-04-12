76f51bd6fef76eb691ce23c74bf89dcf67aa83bc
poovarasijeevabalan <poovarasijeevabalan@gmail.com>
2024-04-12 13:12:53 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+This is a simple calculator that will perform addition, subtraction, multiplication, division, floor divsion and modules 
+
+
+
+ classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+    
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Education",
+  "Operating System :: Microsoft :: Windows :: Windows 10",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: Python :: 3"
+]
+
+setup(
+    name='math_operations',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A simple Python library for basic math operations',
+    long_description=open('README.txt').read() + "\n\n" + open("change_log.txt").read(),
+    author='POOVARASI N',
+    author_email='poovarasi5405@gmail.com',
+    url='https://github.com/yourusername/math_operations',
+    license="MIT",
+    classifiers=classifiers,
+    keywords='calculator',
+    install_requires=[]
+)
